c02ff38f1a9f03f111050eb54e30363cbf24dc66
thislooksfun <thislooksfun@repbot.org>
2014-06-23 18:33:55 -0500

    Updated version number to 1.0.2

--- CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
+++ CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
@@ -20,7 +20,7 @@ public class CamoSuitMain
 {	
 	public static final String MODID = "camosuits";
 	public static final String NAME = "Camo Suits";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	
 	@Instance(CamoSuitMain.MODID)
 	public static CamoSuitMain instance;

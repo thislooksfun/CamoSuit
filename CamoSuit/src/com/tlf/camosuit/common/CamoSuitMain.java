e301a5e9e614c0cf8785f865d037e3c60faa1e93
thislooksfun <thislooksfun@repbot.org>
2014-06-27 22:39:23 -0500

    Updated to version 1.0.3 Added support for http://github.com/thislooksfun/forgeupdater

--- CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
+++ CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
@@ -21,7 +21,7 @@ public class CamoSuitMain
 {	
 	public static final String MODID = "camosuits";
 	public static final String NAME = "Camo Suits";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	
 	@Instance(CamoSuitMain.MODID)
 	public static CamoSuitMain instance;

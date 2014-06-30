2938954dddc4f6dbb72f214d780c57863899d7e3
thislooksfun <thislooksfun@repbot.org>
2014-06-30 18:20:24 -0500

    Updated to 1.0.5

--- CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
+++ CamoSuit/src/com/tlf/camosuit/common/CamoSuitMain.java
@@ -21,7 +21,7 @@ public class CamoSuitMain
 {	
 	public static final String MODID = "camosuits";
 	public static final String NAME = "Camo Suits";
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 	
 	@Instance(CamoSuitMain.MODID)
 	public static CamoSuitMain instance;
